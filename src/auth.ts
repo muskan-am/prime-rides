ef71a5383dbce4b59e9864106779e61a922073c8
Muskan Kesharwani <muskankesharwani63@gmail.com>
2026-08-26 12:05:22 +0530

    connected with db

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
   providers: [
